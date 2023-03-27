9eb3d7f254bfbf0ba942f7b58ebcd9759ba9da4c
Elena Lebedeva <lena4lebedeva@gmail.com>
2023-03-27 20:30:20 +0400

    first commit

---
+++ src/test/java/FilmsManagerTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class FilmsManagerTest {
+
+    @Test
+    public void test() {
+        FilmsManager manager = new FilmsManager();
+
+        manager.addFilm("Film I");
+        manager.addFilm("Film II");
+        manager.addFilm("Film III");
+
+        String[] expected = {"Film I", "Film II", "Film III"};
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void testLast() {
+        FilmsManager manager = new FilmsManager();
+
+        manager.addFilm("Film I");
+        manager.addFilm("Film II");
+        manager.addFilm("Film III");
+
+
+        String[] expected = {"Film III", "Film II", "Film I"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+}
