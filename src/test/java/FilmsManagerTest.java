2f7f33a578185769c1c9e3499430b8edbe0f3833
Elena Lebedeva <lena4lebedeva@gmail.com>
2023-03-28 19:13:36 +0400

    first commit

--- src/test/java/FilmsManagerTest.java
+++ src/test/java/FilmsManagerTest.java
@@ -1,34 +1,38 @@
-import org.junit.Test;
+
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class FilmsManagerTest {
 
-    @Test
-    public void test() {
-        FilmsManager manager = new FilmsManager();
 
-        manager.addFilm("Film I");
-        manager.addFilm("Film II");
-        manager.addFilm("Film III");
+   @Test
+        public void test() {
+            FilmsManager manager = new FilmsManager();
 
-        String[] expected = {"Film I", "Film II", "Film III"};
-        String[] actual = manager.findAll();
+            manager.addFilm("Film I");
+            manager.addFilm("Film II");
+            manager.addFilm("Film III");
 
-        Assertions.assertArrayEquals(expected, actual);
-    }
-    @Test
-    public void testLast() {
-        FilmsManager manager = new FilmsManager();
+            String[] expected = {"Film I", "Film II", "Film III"};
+            String[] actual = manager.findAll();
 
-        manager.addFilm("Film I");
-        manager.addFilm("Film II");
-        manager.addFilm("Film III");
+            Assertions.assertArrayEquals(expected, actual);
+        }
+        @Test
+        public void testLast() {
+            FilmsManager manager = new FilmsManager();
+
+            manager.addFilm("Film I");
+            manager.addFilm("Film II");
+            manager.addFilm("Film III");
 
 
-        String[] expected = {"Film III", "Film II", "Film I"};
-        String[] actual = manager.findLast();
+            String[] expected = {"Film III", "Film II", "Film I"};
+            String[] actual = manager.findLast();
 
-        Assertions.assertArrayEquals(expected, actual);
-    }
+            Assertions.assertArrayEquals(expected, actual);
+        }
+
 
 }
